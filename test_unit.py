116467eaf4ba47de3f7b63b85f78c49d2845183c
Valera <valera.peshchenko@gmail.com>
2018-03-12 20:47:09 +0300

    h6

---
+++ test_unit.py
@@ -0,0 +1,15 @@
+import gather
+import unittest
+
+class SysPython(unittest.TestCase):
+	def setUp(self):
+		'''Init'''
+
+	def test_json_function(self):
+		self.assertFalse(gather.json_file());
+
+	def test_yaml_function(self):
+		self.assertFalse(gather.yaml_file());
+
+	def tearDown(self):
+		'''Finish'''
