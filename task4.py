f49fca0e5ff32a2347dcde862a4d72da2d05fc2f
valerapeshchenko <valera.peshchenko@gmail.com>
2018-03-16 18:53:04 +0300

    Normal task4.py

--- task4.py
+++ task4.py
@@ -1,84 +1,68 @@
 #!/usr/bin/python
-n = input();
-m = input();
-listA=[];
-listB=[];
+import re
+'''block of getting n and m'''
 
-print("put words of group A:");
-for i in range(n):
-	#global listA
-	word=input();
-	listA.append(word);
+print("Getting integers n and m:")
+input_string = input()
+n = 0
+m = 0
+result = re.search(r'(\d{1,4}|10000) (\d{1,2}|100)', input_string)
+if result:
+    print("good input string!")
+    n = re.findall(r'\d+', input_string)
+else:
+    print("Bad input string!")
+    exit()
 
-print("put words of group B:");
-for y in range(m):
-	#global listB
-        word = input();
-        listB.append(str(word));
+'''block of getting n and m'''
+listA = []
+listB = []
 
-print("listA:");
-print(listA);
+print("put words of group A:")
+for i in range(int(n[0])):
+    # global listA
+    word = input()
+    listA.append(word)
 
-print("listB:");
-print(listB);
+print("put words of group B:")
+for y in range(int(n[1])):
+    # global listB
+    word = input()
+    listB.append(str(word))
 
-#for i in range(len(new_list)):
-#	print(get_key(new_list[i]));
-#listA=['a','a','c','c','b','d','e','b','b','e','a','a','a','e','b','e','d','d'];
-#listB=['a','e','c','d','b'];
-c={}
+
+c = {}
 for g in range(len(listB)):
         for i in range(len(listA)):
-                #global listA 
-                #global listB
-                #global c
-                if listA[i] == listB[g]:                   
-                        c[i]=str(listA[i]);
-
-for i in c.items():
-    print i
-keys=[];
-keys.append(c.keys());
-print(keys[0]);
-new_list=c.values();
-
-jump=0;
-print(new_list);
-i=0;
-cycle=len(new_list)-1;
-old_string=c.values();
-countB=0;
-while i < cycle:
-	#if i >= cycle:
-	#	break;
-	#global c
-	stop=0
-	jump=0;
-	outPutString=str(keys[0][i]);
-	for g in range(i + 1,len(new_list)):
-		#global stop
-		#global jump
-		if new_list[i] != new_list[g]:
-			stop=1
-		if stop == 0:
-			jump += 1;
-		if new_list[i] == new_list[g]:	
-			outPutString += " ";		
-			outPutString += str(keys[0][g]);
-			outPutString += " ";	
-	i += jump;
-	i += 1;
-	if countB < len(listB):
-		countB += 1;	
-		print(outPutString);
-
-
-
-
-
-
-
-
-
-
-
+                # global listA
+                # global listB
+                # global c
+                if listA[i] == listB[g]:
+                        c[i] = str(listA[i])
+keys = []
+keys.append(list(c.keys()))
+new_list = list(c.values())
+jump = 0
+i = 0
+cycle = len(new_list)-1
+old_string = c.values()
+countB = 0
+while i <= cycle:
+    stop = 0
+    jump = 0
+    outPutString = str(keys[0][i] + 1) + " "
+    for g in range(i + 1, len(new_list)):
+        # global stop
+        # global jump
+        if new_list[i] != new_list[g]:
+            stop = 1
+        if stop == 0:
+            jump += 1
+        if new_list[i] == new_list[g]:
+            outPutString += str(keys[0][g] + 1)
+            outPutString += " "
+    i += jump
+    i += 1
+    if countB < len(listB):
+        countB += 1
+    print(outPutString)
